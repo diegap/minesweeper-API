fff0501fd3158fce23dcfe1d54495412aec16040
Diego Perez <diego.perez@etermax.com>
2020-09-20 21:00:41 -0300

    Define Cell family

--- src/main/kotlin/com/deviget/minesweeper/core/domain/entities/Cell.kt
+++ src/main/kotlin/com/deviget/minesweeper/core/domain/entities/Cell.kt
@@ -2,6 +2,8 @@ package com.deviget.minesweeper.core.domain.entities
 
 import com.deviget.minesweeper.core.domain.entities.CellVisibility.HIDDEN
 import com.deviget.minesweeper.core.domain.entities.CellVisibility.VISIBLE
+import com.deviget.minesweeper.core.domain.exceptions.CellCannotBeRevealedException
+import com.deviget.minesweeper.core.domain.exceptions.GameOverException
 
 enum class CellVisibility {
 	HIDDEN,
@@ -10,17 +12,34 @@ enum class CellVisibility {
 
 data class CellValue(val value: Int)
 
+interface CellBehaviour {
+	fun reveal(): Int
+}
+
+class MinedCell(val cell: CellBehaviour) : CellBehaviour {
+	override fun reveal() = throw GameOverException()
+}
+
+class FlaggedCell(val cell: CellBehaviour) : CellBehaviour {
+	override fun reveal() = throw CellCannotBeRevealedException()
+}
+
+class QuestionMarkedCell(val cell: CellBehaviour) : CellBehaviour {
+	override fun reveal() = cell.reveal()
+}
+
 class Cell(
 		val position: Position,
 		private var visibility: CellVisibility = HIDDEN,
 		val cellValue: CellValue
-) {
+) : CellBehaviour {
 
 	val adjacentPositions get() = position.adjacentPositions
 	val value get() = cellValue.value
 
-	fun reveal() {
+	override fun reveal(): Int {
 		visibility = VISIBLE
+		return cellValue.value
 	}
 
 	fun isVisible() = visibility == VISIBLE
