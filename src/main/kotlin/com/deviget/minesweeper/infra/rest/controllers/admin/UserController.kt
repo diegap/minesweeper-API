e407ce7ee453117f1f8a84af06efaef56d43808d
Diego Perez <diego.perez@etermax.com>
2020-09-25 15:31:01 -0300

    Create user controller

---
+++ src/main/kotlin/com/deviget/minesweeper/infra/rest/controllers/admin/UserController.kt
@@ -0,0 +1,18 @@
+package com.deviget.minesweeper.infra.rest.controllers.admin
+
+import com.deviget.minesweeper.core.actions.CreateUser
+import com.deviget.minesweeper.infra.rest.representations.UserRepresentation
+import org.springframework.http.HttpStatus.CREATED
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class UserController(private val createUser: CreateUser) {
+
+	@PostMapping("/admin/users")
+	fun createUser(@RequestBody userRepresentation: UserRepresentation) = run {
+		createUser(userRepresentation.toDomain())
+		CREATED
+	}
+}
