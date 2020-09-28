ad35e0d46ce01856256a65b7c8bf648349b15008
Diego Perez <diego.perez@etermax.com>
2020-09-27 23:43:00 -0300

    Allow all methods in CORS config

--- src/main/kotlin/com/deviget/minesweeper/infra/config/WebConfiguration.kt
+++ src/main/kotlin/com/deviget/minesweeper/infra/config/WebConfiguration.kt
@@ -11,5 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 open class WebConfiguration : WebMvcConfigurer {
 	override fun addCorsMappings(registry: CorsRegistry) {
 		registry.addMapping("/**")
+				.allowedMethods("*")
 	}
 }
